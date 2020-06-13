89d23c6a6b0007b819899539a972a6e4974c556c
Tyler <twit96@users.noreply.github.com>
2020-06-13 16:58:26 -0500

    TOC and Blog Framework
    Added '/blog/index.html' landing page. Updated navigation bar to link to page.
    
    Added 'toc.css' and 'toc.js' files for dynamically generated Table of Contents from a page's h3 elements.
    
    Implemented new TOC in '/blog/drafts/2019-09-16-TankFighter/index.html'. Added link to this article on 'blog' page.
    
    Added empty 'index.html' files to 'blog/drafts/' subfolders.
    
    Pages accessed from a main navigation page now use the navigation bar of the page. For instance, 'Blog' will still be highlighted on blog post pages, and 'Portfolio' is still highlighted on Star Wars Survey project page.

---
+++ assets/js/toc.js
@@ -0,0 +1,30 @@
+$(document).ready( function(){
+  var ToC ="<div class='table-of-contents'" +
+    "<h3>Table of Contents:</h3>" +
+    "<ul>";
+
+  var newLine, el, title, link;
+
+  $("h3").each(function() {
+
+    el = $(this);
+    title = el.text();
+    link = "#" + el.attr("id");
+
+    newLine =
+      "<li>" +
+        "<a href='" + link + "'>" +
+          title +
+        "</a>" +
+      "</li>";
+
+    ToC += newLine;
+
+  });
+
+  ToC +=
+     "</ul>" +
+    "</div>";
+
+    $(".sections").prepend(ToC);
+ });
