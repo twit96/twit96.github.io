68b2f945a52be57bb4017b29721de8bd87ed0212
Tyler Wittig <tylerwittig@utexas.edu>
2020-11-17 23:36:30 -0600

    Support for 'prefers-color-scheme: dark'
    changed name of root 'dark' class to 'invert-color-scheme'.
    
    added media query for user dark mode, in which the root element color variables and 'invert-color-scheme; variables are swapped.
    
    This commit allows users to toggle between color schemes, but if they prefer dark mode, they start with dark mode.

--- assets/js/toggles.js
+++ assets/js/toggles.js
@@ -1,9 +1,12 @@
 // Allow User to Toggle Page-Wide Dark Mode
 $('.slider').on('click', function() {
 
-  if ($(':root').hasClass('dark')) {
-    $(':root').removeClass('dark');
+  // dark theme on root element
+  if ($(':root').hasClass('invert-color-scheme')) {
+    $(':root').removeClass('invert-color-scheme');
   } else {
-    $(':root').addClass('dark');
+    $(':root').addClass('invert-color-scheme');
   }
+
+  // $('#intro').find('img').toggle();
 });
