447319152caea07b4a7f4d8e0111c931c126b8d6
Tyler Wittig <tylerwittig@utexas.edu>
2020-11-27 00:55:44 -0600

    New Header
    Added new header to homepage and updated footer.
    Added header.js file to provide functionality to homepage header.
    
    Updated header.html for all non-homepage pages. Removed old header from all site pages, and its associated masthead.css file. Removed references to masthead.css from all pages.
    
    Removed resume page and its associated directory (you will be missed).

---
+++ assets/js/header.js
@@ -0,0 +1,78 @@
+// All Nav Links Click Behavior - Scroll to position on page ------------------
+
+$.fn.scrollView = function () {
+    return this.each(function () {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top - 75 + 'px'
+        }, 500);
+    });
+}
+
+$('header a').click(function() {
+  var loc = '#' + $(this).attr('class');
+  $(loc).scrollView();
+
+  // set active link
+  $(this).toggleClass('active');
+  $('header a').not(this).removeClass('active');
+
+  // delay then remove active link
+  $(this).addClass('active').delay(2000).queue(function(next){
+    $(this).removeClass('active');
+    next();
+  });
+
+});
+
+$('header .title').click(function() {
+  $('#intro').scrollView();
+});
+
+
+// Toggle Small Nav on Hamburger Icon Click -----------------------------------
+$('.hamburger-icon').click(function() {
+  // update button
+  $(this).toggleClass('wasClicked');
+  // display small nav
+  $('#small-nav').toggle();
+  // display small nav background
+  $('#small-nav-bg').fadeToggle();
+});
+
+
+// Hide Small Nav... ----------------------------------------------------------
+function hide_small_nav() {
+  // reset hamburger icon
+  $('.hamburger-icon').toggleClass('wasClicked');
+  // slide up small nav
+  $('#small-nav').toggle();
+  // fade out small nav background
+  $('#small-nav-bg').fadeOut();
+}
+
+// ...when a link is clicked
+$('#small-nav a').click(function() {
+  hide_small_nav();
+});
+
+// ...if user clicks somewhere else
+$('#small-nav-bg').click(function() {
+  hide_small_nav();
+});
+
+// ...if title link is clicked
+$('header .title').click(function() {
+  if ($('.hamburger-icon').hasClass("wasClicked")) {
+    hide_small_nav();
+  }
+});
+
+// ...if window grows large enough to display full nav
+$(window).resize(function() {
+  if($(window).width() >= 700) {
+    // detect if small nav is currently displaying
+    if ($('.hamburger-icon').hasClass("wasClicked")) {
+      hide_small_nav();
+    }
+  }
+});
