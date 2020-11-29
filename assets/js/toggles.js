e1b431a4e92fc57a2fd78488c51d4c60759a3df3
Tyler Wittig <tylerwittig@utexas.edu>
2020-11-28 22:26:28 -0600

    Implemented Light/Dark Mode Toggle
    Added to homepage header bar in small nav and in full nav.
    
    Added javascript in toggle.js to ensure both toggles maintain same toggle state.
    
    Updated header.html

--- assets/js/toggles.js
+++ assets/js/toggles.js
@@ -1,12 +1,22 @@
 // Allow User to Toggle Page-Wide Dark Mode
 $('.slider').on('click', function() {
 
-  // dark theme on root element
+  // toggle dark theme on root element
   if ($(':root').hasClass('invert-color-scheme')) {
     $(':root').removeClass('invert-color-scheme');
   } else {
     $(':root').addClass('invert-color-scheme');
   }
 
+  // toggle inverted color scheme on contact section link images
+  // (tempfix until SVGs implemented here)
+  $('.link-card img').toggleClass('inverted');
+
+  // update slider from other nav (since separate sliders in small/full navs)
+  var this_input = $(this).parent().children('input:checkbox');
+  var this_checked = !this_input.prop('checked');
+  $('input:checkbox').not(this_input).prop('checked', this_checked);
+
+  // swap intro image
   // $('#intro').find('img').toggle();
 });
