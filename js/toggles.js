db20874d428ac27e60210143bbb2913c33ef9193
Tyler Wittig <tylerwittig@utexas.edu>
2020-12-04 14:36:19 -0600

    Added nav page toggle for all-projects
    added hidden page toggle to nav that is swapped with other nav links alongside all-projects content swap. (Gives clearer route back to homepage content, even though all-projects page content does already have a home button.)

--- js/toggles.js
+++ js/toggles.js
@@ -34,9 +34,19 @@ $('.slider').on('click', function() {
 */
 $('.pages-toggle').on('click', function() {
   // toggle header elements
-  $('header .projects').fadeToggle();
-  $('header .experience').fadeToggle();
-  $('header .contact').fadeToggle();
+
+  if ($('header a.pages-toggle').is(":visible")) {
+    $('header a.pages-toggle').toggle();
+    $('header a').not('.title').not('.pages-toggle').fadeToggle();
+
+  } else {
+    $('header a').not('.title').not('.pages-toggle').toggle();
+    $('header a.pages-toggle').fadeToggle();
+  }
+
+
+  // $('header .experience').fadeToggle();
+  // $('header .contact').fadeToggle();
   // toggle page elements
   $('#intro').toggle();
   $('article').toggle();
