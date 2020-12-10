220ff62aa248f1543699bfc6e28b8f1b1031aaae
Tyler Wittig <tylerwittig@utexas.edu>
2020-12-09 18:20:18 -0600

    404 Page Tweaks, Animated Background Start Fix
    Added animations to 404 page
    
    Tweaked Background Animation so that animation delays are negative seconds instead of positive seconds - prevents whitespace on first few seconds of page load

---
+++ js/404.js
@@ -0,0 +1,17 @@
+// Timer Config
+var timer = document.getElementById('timer');
+var time = 10;
+
+function updateTimer() {
+  timer.innerHTML = time;
+  if (time == 0) {
+    // Simulate an HTTP redirect:
+    window.location.replace("/");
+  }
+  time --;
+  // asynchronous pause, then recursive call
+  setTimeout(() => { updateTimer() }, 1000);
+}
+
+// Initial Call
+updateTimer();
