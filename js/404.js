3d66d665ec4daf2d5b3b4f4619deb10a2bf85a03
Tyler Wittig <tylerwittig@utexas.edu>
2020-12-13 17:13:19 -0600

    404 Page Links Redirect
    To prevent back button after exiting page

--- js/404.js
+++ js/404.js
@@ -1,18 +1,25 @@
-// Timer Config
+/**
+* All links Simulate an HTTP redirect - no back button
+*/
+$('#not-found a').on('click', function() {
+  window.location.replace("/");
+});
+
+
+/**
+* Timer Config - Delayed Forced Redirect
+*/
 var timer = document.getElementById('timer');
 var time = 10;
 
 function updateTimer() {
   timer.innerHTML = time;
   if (time == 0) {
-    // Simulate an HTTP redirect:
     window.location.replace("/");
   } else {
     time --;
-    // asynchronous pause, then recursive call
     setTimeout(() => { updateTimer() }, 1000);
   }
 }
 
-// Initial Call
 updateTimer();
